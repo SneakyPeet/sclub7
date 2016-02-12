100615befaea2fa75ea0c08863ca1e63961e31ee
Pieter Koornhof <pieter@entelect.co.za>
2016-02-12 09:04:39 +0200

    process only for the current year

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var argv = require('yargs').argv;
 var fs = require('fs');
+var _ = require('lodash');
 var sync = require('./src/sync.js');
 var yearToDate = require('./src/yearToDate.js');
 var overall = require('./src/overall.js');
 
 sync(argv.token, argv.club, function(err, activities) {
   checkErr(err)
-  saveToFile('./data/ytd.json', yearToDate(activities));
-  saveToFile('./data/overall.json', overall(activities));
+  var activitiesForCurrentYear = getActivitiesForCurrentYear(activities);
+  saveToFile('./data/ytd.json', yearToDate(activitiesForCurrentYear));
+  saveToFile('./data/overall.json', overall(activitiesForCurrentYear));
 });
+
+function getActivitiesForCurrentYear(activities) {
+  var fromDate = new Date(new Date().getFullYear(), 0, 1);
+  return _.filter(activities, function(a) {
+    return new Date(a.start_date) >= fromDate;
+  });
+}
 
 function saveToFile(fileName, data) {
   fs.writeFile(fileName, JSON.stringify( data, null, 2 ), "utf8", function(err) {
